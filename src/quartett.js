a885a66dbcea95b425e5fde320b277e3a2df6d15
alexroesner <alexander.r.ars@gmail.com>
2018-10-05 12:49:36 +0200

    aktualität eingefügt

--- src/quartett.js
+++ src/quartett.js
@@ -2,8 +2,10 @@
 
 var result =[];
 var i;
-var k=13;
+var k=12;
 var kombi;
+var data_a
+
 
 function init () {
     console.log('Initialize our app');
@@ -21,15 +23,12 @@ function init () {
         return response.json();
       })
       .then(function (data) {
-        console.log(data)
-        for(i=0;i<32;i++){
-        //   if(data[Math.floor(Math.random()*data.length)].name)
-        //   result[i]=
-        }
-        console.log(result)
+        console.log(data);
+        console.log(result);
         const card = document.getElementById('card-template');
-        var card2 =new card;
-        //for(k=0;k<2;k++){
+        for(i=0;i<32;i++){
+          result[i]=data[Math.floor(Math.random()*data.length)];
+        }
           document.getElementById("name").innerHTML=result[k].name;
 
           var x=(k%4)+1;
@@ -102,11 +101,32 @@ function init () {
           document.getElementById("name2").innerHTML=result[k-x+2].name;
           document.getElementById("name3").innerHTML=result[k-x+3].name;
           document.getElementById("name4").innerHTML=result[k-x+4].name;
-          document.getElementById("wert1").innerHTML='Anzahl der Sensoren='+result[k].sensors.length;
-          document.getElementById("wert2").innerHTML='Aktualität';
-          //TODO auktualität
-          document.getElementById("wert2").innerHTML='?';
 
+          document.getElementById("wert1").innerHTML='Anzahl der Sensoren='+result[k].sensors.length;
+          //TODO auktualität
+          var updatedAt=new Date(result[k].updatedAt.substring(0,4),
+                                (result[k].updatedAt.substring(5,7)-1),
+                                result[k].updatedAt.substring(8,10),
+                                result[k].updatedAt.substring(11,13),
+                                result[k].updatedAt.substring(14,16),
+                                result[k].updatedAt.substring(17,19)
+                                )
+          var jetzt=new Date();
+          
+          var aktualität=(jetzt-updatedAt)/3600000;
+          if (aktualität>24){
+            aktualität=aktualität/24;
+            document.getElementById("wert2").innerHTML='Tage seit der letzten Aktualisierung: '+Math.floor(aktualität);
+          }
+          else{
+            document.getElementById("wert2").innerHTML='Stunden seit der letzten Aktualisierung: '+Math.floor(aktualität);
+          }
+          
+          
+          console.log(jetzt);
+          console.log(updatedAt);
+          console.log(aktualität);
+          // document.getElementById()
         })
       
         
